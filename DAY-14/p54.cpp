ad1d5e3b4c8d959a948fc74eb6e7624bc38c1d8e
krishna gupta <kgap6677@gmail.com>
2026-06-25 16:36:12 +0530

    Add DAY-14 files

---
+++ DAY-14/p54.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key, count = 0;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Enter element: ";
+    cin >> key;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key)
+            count++;
+    }
+
+    cout << "Frequency of " << key << " = " << count;
+
+    return 0;
+}
